0edd4896049740d422eff555500d6b99b0aca7eb
ChloeBarbier <chloe.barbier.insa@gmail.com>
2022-06-28 22:43:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { FaLinkedin } from 'react-icons/fa';
+import { BsTelephoneFill, BsGithub } from 'react-icons/bs';
+import { GrMail } from 'react-icons/gr';
+import Main from './Main';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className='content-header'>
+            <div className='container'>
+              <div className="title">
+                <Link className="main-title" to="/">
+                  <img src="/assets/img/favicon.png" alt="icon" />
+                  Chloé Barbier
+                </Link>
+                {/* <div className="sub-title">Photographie</div> */}
+              </div>
+              <nav className="menu">
+                <Link className="menu-item" to="/">Accueil</Link>
+                <Link className="menu-item" to="gallery">Galerie</Link>
+                <Link className="menu-item" to="about">À propos</Link>
+                <Link className="menu-item" to="contact">Contact</Link>
+              </nav>
+            </div>
+          </div>
+        </header>
+        <Main />
+        <footer className="App-footer">
+          <div className='content-footer'>
+            <div className='container'>
+              <div className="author">© Chloé Barbier - Mise à jour 28 juin 2022</div>
+              <div className="social-media">
+                <a className="social-media-link" href="tel:+33650966461" target="_blank" rel="noreferrer">
+                  <BsTelephoneFill className="social-media-icons"/>
+                </a>
+                <a className="social-media-link" href="https://www.linkedin.com/in/chloe-barbier/" target="_blank" rel="noreferrer">
+                  <FaLinkedin className="social-media-icons"/>
+                </a>
+                <a className="social-media-link" href="mailto:chloe.barbier.insa@gmail.com" target="_blank" rel="noreferrer">
+                  <GrMail className="social-media-icons"/>
+                </a>
+                <a className="social-media-link" href="https://www.github.com/ChloeBarbier" target="_blank" rel="noreferrer">
+                  <BsGithub className="social-media-icons"/>
+                </a>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
